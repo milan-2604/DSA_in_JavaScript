6efb811f252f4a4c65eca3f0e9ba25be2c94f64e
milan-2604 <milan2003oli@gmail.com>
2025-09-08 11:29:38 +0530

    completed singly linked list now to do is doubly linked list

--- Linked List/singlyLinkedLilst.js
+++ Linked List/singlyLinkedLilst.js
@@ -6,7 +6,7 @@ class Node{
     }
 }
 
-//LinkedList clsss -> manages list
+//LinkedList class -> manages list
 class LinkedList{
     constructor(){
         this.head = null;
@@ -22,7 +22,7 @@ class LinkedList{
         else{
             let current = this.head; //if head exists lets put current on the first node
             while(current.next){
-                current=current.next //incrementing current until it reaches last node
+                current=current.next; //incrementing current until it reaches last node
             }
             current.next = newNode; //assigning address of newly created node to the last node
         }
@@ -32,7 +32,6 @@ class LinkedList{
     //pop() deleting last element from the list
     pop(){
         if(!this.head){
-            console.log("list is empty nothing to pop");
             return undefined;
         }
 
@@ -51,6 +50,173 @@ class LinkedList{
         previous.next=null;
         this.length--;
         return current.value;
+    }
+
+    //unshift() adding item in front of the list
+    unshift(value){
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head=newNode;
+        this.length++;
+    }
+
+    //shift() deleting first element from front of the list
+    shift(){
+        if(!this.head){
+            return undefined;
+        }
+        let current = this.head;
+        this.head = current.next;
+        this.length--;
+        return current.value;
+    }
+
+    //getFirst() returning the first item value of the list
+    getFirst(){
+        if(!this.head){
+            return undefined;
+        }
+        return this.head.value;
+    }
+    
+    //getLast() returning last item value of the list
+    getLast(){
+        if(!this.head){
+            return undefined;
+        }
+        let current = this.head;
+        while(current.next){
+            current=current.next;
+        }
+        return current.value
+    }
+
+    //get() get element by index
+    get(index){
+        if(!this.head){
+            return undefined;
+        }
+        if(index>=this.length|| index<0){
+            return false;
+        }
+        let current=this.head;
+        let currentIndex =0;
+        while(currentIndex!=index){
+            current=current.next;
+            currentIndex++;
+        }
+        return current.value;
+    }
+
+    //set(index,value) set value at specific index without altering LinkedList
+    set(index,value){
+        if(index>=this.length || index<0){
+            return false;
+        }
+        let current = this.head;
+        let currentIndex = 0;
+        while(currentIndex!=index){
+            current = current.next;
+            currentIndex++;
+        }
+        current.value=value;
+        return true;
+    }
+
+    //insert(index,value) inserting a newNode at desired index
+    insert(index,value){
+       
+        if(index>this.length || index<0){
+            return false;
+        }
+        if(index===0){
+            this.unshift(value);
+            return true;
+        }
+        if(index===this.length){
+            this.push(value)
+            return true;
+        }
+         const newNode = new Node(value);
+        let current=this.head;
+        let currentIndex=0;
+        let previous = null;
+        while(currentIndex!=index){
+            previous=current;
+            current=current.next;
+            currentIndex++;
+        }
+        previous.next=newNode;
+        newNode.next=current;
+        this.length++;
+        return true;
+    }
+
+    //size() total no. of nodes
+    size(){
+        return this.length;
+    }
+
+    //clear() delete all nodes
+    clear(){
+        this.head=null;
+        this.length=0;
+    }
+
+    //remove(index) remove node from specific index
+    remove(index){
+        if(index>=this.length || index<0){
+            return false;
+        }
+        if(index===0){
+           return this.shift();
+        }
+        if(index=== this.length-1){
+           return this.pop();
+        }
+        let current= this.head;
+        let previous= null;
+        let currentIndex = 0;
+        while(currentIndex!=index){
+            previous=current;
+            current=current.next;
+            currentIndex++;
+        }
+        previous.next=current.next;
+        this.length--;
+        return current.value;
+    }
+
+    //toArray() copying linked list values in an array
+    toArray(){
+        const arr = [];
+        if(!this.head){
+            return arr ;
+        }
+        
+        let current=this.head;
+        while(current){
+            arr.push(current.value);
+            current=current.next;
+        }
+        return arr;
+    }
+
+    //reverse() reversing the refrence of each node
+    reverse(){
+        if(!this.head){
+            return undefined;
+        }
+        let current=this.head;
+        let previous=null; 
+        
+        while(current!=null){
+            let next = current.next;
+            current.next = previous
+            previous=current;
+            current=next;
+        }        
+        this.head=previous;                                        
     }
 
     //print() printing the linked list
@@ -77,6 +243,14 @@ const newList = new LinkedList();
 newList.push(50);
 newList.push(100);
 newList.push(150);
-newList.print()
-newList.pop();
+// newList.print()
+// newList.unshift(40)
+// newList.pop();
+// newList.print();
+// newList.shift();
+// newList.print()
+// console.log(newList.getLast())
+// console.log(newList.get(1))
+// console.log(newList.toArray())
+newList.reverse();
 newList.print();
