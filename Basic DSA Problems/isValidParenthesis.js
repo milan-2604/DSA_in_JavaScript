eeec34f90edd08809312d6e11f6f2097fe474bfc
milan-2604 <milan2003oli@gmail.com>
2025-09-10 20:17:17 +0530

    isValidParenthesis using stack done

---
+++ Basic DSA Problems/isValidParenthesis.js
@@ -0,0 +1,40 @@
+// const isValidParenthesis = (str)=>{
+//     const stack = [];
+//     const data ={
+//     ')':"(",
+//     "}":"{",
+//     "]":"["
+//     };
+//     for(let char of str ){
+//         if(char==='('|| char==="{" || char==="["){
+//             stack.push(char);
+//         }else{
+//             if(stack.length===0 || stack.pop() !== data[char]){
+//                 return false;
+//             }
+//         }
+//     }
+//     return stack.length===0;
+// }
+
+
+//other version when we reverse key value pairs
+const isValidParenthesis = (str)=>{
+    const stack = [];
+    const data ={
+    "(":')',
+    "{":"}",
+    "[":"]"
+    };
+    for(let char of str ){
+        if(data[char]){
+            stack.push(data[char]);
+        }else{
+            if(stack.pop()!==char){
+                return false
+            }
+        }
+    }
+    return stack.length===0;
+}
+console.log(isValidParenthesis("{()}[]"));
