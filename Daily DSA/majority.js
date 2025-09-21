4708646fde163dcf76f393eb987cf5cc2a04bd11
milan-2604 <milan2003oli@gmail.com>
2025-09-21 19:41:56 +0530

    just learned Boyer's majority algo but it's only applicable if majority element is more than n\2

---
+++ Daily DSA/majority.js
@@ -0,0 +1,52 @@
+// 169. Majority Element
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+ 
+
+// Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate = nums[0];
+    let counter = 1;
+    for(let i=1; i<nums.length; i++){
+        if(candidate===nums[i]){
+            counter++;
+        }else{
+            counter--;
+            if(counter===0){
+                candidate=nums[i];
+                counter=1;
+            }
+        }
+    }
+    return candidate;
+};
+
+console.log(majorityElement([2,2,3,2,3,4,2,]));
