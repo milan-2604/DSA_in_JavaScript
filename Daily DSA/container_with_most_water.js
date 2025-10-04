c870bdf4bcbcc1491c34f8177a3f1c78d7b3774c
milan-2604 <milan2003oli@gmail.com>
2025-10-04 11:21:12 +0530

    easier than expected

--- Daily DSA/container_with_most_water.js
+++ Daily DSA/container_with_most_water.js
@@ -1,16 +1,26 @@
- let a = 0;
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let a = 0;
     let b = height.length-1;
     let result = 0;
     while(a!==b){
         if(height[a]<height[b]){
             if((b-a)*(height[a]) > result){
                 result=(b-a)*(height[a]);
-                a++;
+                
             }
+            a++;
         }else{
             if((b-a)*(height[b])>result){
                 result=(b-a)*(height[b]);
-                b--;
+                
             }
+            b--;
         }
     }
+return result;
+};
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
