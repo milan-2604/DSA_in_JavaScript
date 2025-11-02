189374b8e1fc9367c77667f45d9f638e0a32b1c5
milan-2604 <milan2003oli@gmail.com>
2025-11-02 19:18:53 +0530

    two pointers 1

---
+++ Leetcode Patterns/Two Pointers/leetcode_283.js
@@ -0,0 +1,46 @@
+/*
+283. Move Zeroes
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+*/
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let a = 0;//tracks zeros
+  for(let b=0 ; b<nums.length; b++){
+    if(nums[b]!==0){
+        [nums[a],nums[b]]=[nums[b],nums[a]];
+        a++;
+    }
+  }
+
+};
