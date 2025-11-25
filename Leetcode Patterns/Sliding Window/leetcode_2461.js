bf14b5d4788fe7270055f99fca4cbe65f353e027
milan-2604 <milan2003oli@gmail.com>
2025-11-25 14:45:45 +0530

    sliding windows 5th done

---
+++ Leetcode Patterns/Sliding Window/leetcode_2461.js
@@ -0,0 +1,76 @@
+/*
+2461. Maximum Sum of Distinct Subarrays With Length K
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of 
+nums that meet the following conditions:
+
+The length of the subarray is k, and
+All the elements of the subarray are distinct.
+Return the maximum subarray sum of all the subarrays that meet the conditions. If no subarray meets the conditions, return 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,4,2,9,9,9], k = 3
+Output: 15
+Explanation: The subarrays of nums with length 3 are:
+- [1,5,4] which meets the requirements and has a sum of 10.
+- [5,4,2] which meets the requirements and has a sum of 11.
+- [4,2,9] which meets the requirements and has a sum of 15.
+- [2,9,9] which does not meet the requirements because the element 9 is repeated.
+- [9,9,9] which does not meet the requirements because the element 9 is repeated.
+We return 15 because it is the maximum subarray sum of all the subarrays that meet the conditions
+Example 2:
+
+Input: nums = [4,4,4], k = 3
+Output: 0
+Explanation: The subarrays of nums with length 3 are:
+- [4,4,4] which does not meet the requirements because the element 4 is repeated.
+We return 0 because no subarrays meet the conditions.
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 105
+1 <= nums[i] <= 105
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maximumSubarraySum = function (nums, k) {
+  let max = 0;
+  let currSum = 0;
+  const freq = new Map();
+  let left = 0;
+  for (let right = 0; right < nums.length; right++) {
+    currSum += nums[right];
+    freq.set(nums[right], (freq.get(nums[right]) || 0) + 1);
+    if (right - left + 1 > k) {
+      currSum -= nums[left];
+      const count = freq.get(nums[left]);
+      if (count === 1) {
+        freq.delete(nums[left]);
+      } else {
+        freq.set(nums[left], count - 1);
+      }
+      left++;
+    }
+    if(right-left+1===k){
+        if(freq.size===k){
+            max = Math.max(currSum,max);
+        }
+    }
+  }
+  return max;
+};
