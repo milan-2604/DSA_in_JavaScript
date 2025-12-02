0328f697d22fcd8aa6a8af48ddc01f9f0d491fe0
milan-2604 <milan2003oli@gmail.com>
2025-12-02 18:36:20 +0530

    started neetocde 150

---
+++ Neetcode_150/Arrays & Hashing/validAnagram.js
@@ -0,0 +1,47 @@
+/*
+242. Valid Anagram
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+*/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  const count = Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s[i].charCodeAt(0) - 97]++;
+    count[t[i].charCodeAt(0) - 97]--;
+  }
+  return count.every((char) => char === 0);
+};
