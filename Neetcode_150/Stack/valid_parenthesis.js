e674b4eb343e9c6dc72b0e7c4799433e39c96c03
milan-2604 <milan2003oli@gmail.com>
2025-12-13 15:18:52 +0530

    stack started

---
+++ Neetcode_150/Stack/valid_parenthesis.js
@@ -0,0 +1,94 @@
+/*
+20. Valid Parentheses
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+*/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  //     const stack = [];
+  //     const data = {
+  //         "(":")",
+  //         "{":"}",
+  //         "[":"]"
+  //     }
+  //   for(let char of s){
+  //     if(data[char]){
+  //         stack.push(data[char]);
+  //     }else{
+  //         if(stack.pop()!==char){
+  //             return false;
+  //         }
+  //     }
+  //   }
+  //   return stack.length===0;
+  let stack = [];
+  let obj = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  for (let sign of s) {
+    if (!obj[sign]) {
+      stack.push(sign);
+    } else {
+      if (stack[stack.length - 1] === obj[sign]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
