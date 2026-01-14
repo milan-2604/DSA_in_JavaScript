470ebcd2eea9d721e7ba3a6e386afe87cb5722c8
milan-2604 <milan2003oli@gmail.com>
2026-01-14 10:57:16 +0530

    add linked list cycle detection implementation

---
+++ Neetcode_150/Linked_List/linked_list_cycle_detection.js
@@ -0,0 +1,72 @@
+/*
+Linked List Cycle Detection
+Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
+
+Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+
+Note: index is not given to you as a parameter.
+
+Example 1:
+
+
+
+Input: head = [1,2,3,4], index = 1
+
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+
+
+
+Input: head = [1,2], index = -1
+
+Output: false
+Constraints:
+
+1 <= Length of the list <= 1000.
+-1000 <= Node.val <= 1000
+index is -1 or a valid index in the linked list.
+
+*/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+  /**
+   * @param {ListNode} head
+   * @return {boolean}
+   */
+  hasCycle(head) {
+    //using hash set but it would use o(n) extra space
+    //     let seen = new Set();
+    //     let curr = head;
+    //     while(curr){
+    //         if(seen.has(curr))return true;
+    //         seen.add(curr);
+    //         curr = curr.next;
+    //     }
+    //     return false;
+
+    //using fast and slow pointer main idea is if they meet then there is a cycle
+    let fast = head;
+    let slow = head;
+
+    //need to check this so we dont accidently do null.next
+    while (fast !== null && fast.next !== null) {
+      fast = fast.next.next; //move fast pointer two node ahead
+      slow = slow.next; //slow to only one pointer ahead
+      if (slow === fast) return true;
+    }
+    return false;
+  }
+}
